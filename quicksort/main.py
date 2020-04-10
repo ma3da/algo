f587e7914d38a741ac6c1d285e0bcb531645e95b
lpc <lpintocastaneda@gmail.com>
2020-04-10 15:43:46 +0200

    comparable qsort

--- quicksort/main.py
+++ quicksort/main.py
@@ -1,4 +1,27 @@
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
 def quicksort(arr, lo, hi):
+    if lo < hi:
+        mid = partition(arr, lo, hi)
+        quicksort(arr, lo, mid - 1)
+        quicksort(arr, mid + 1, hi)
+
+
+def partition(arr, lo, hi):
+    pivot = arr[hi]
+    i = lo - 1
+    for j, e in enumerate(arr[lo:hi], start=lo):
+        if e <= pivot:
+            i += 1
+            swap(arr, i, j)
+    i += 1
+    swap(arr, i, hi)
+    return i
+
+
+def quicksort2(arr, lo, hi):
     if lo < hi:
         mid = (lo + hi) // 2
         pivot = arr[mid]
@@ -11,8 +34,8 @@ def quicksort(arr, lo, hi):
             else:
                 sups.append(e)
         arr[lo:hi + 1] = subs + pivs + sups
-        quicksort(arr, lo, lo + len(subs))
-        quicksort(arr, lo + len(subs) + len(pivs), hi)
+        quicksort2(arr, lo, lo + len(subs))
+        quicksort2(arr, lo + len(subs) + len(pivs), hi)
 
 
 def read_numbers(file_path="numbers.txt"):
