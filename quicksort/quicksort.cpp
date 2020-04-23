3bd4b5ebeac2f6b23bc00bdc350d7d515c345154
lpc <lpintocastaneda@gmail.com>
2020-04-23 23:56:12 +0200

    more modularity cpp + argparse run

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -1,33 +1,13 @@
-#include <iostream>
-#include <fstream>
 #include <vector>
+#include "utils.h"
 
-void swap(std::vector<int>&, const int&, const int&);
 void quicksort(std::vector<int>&, const int&, const int&);
 int partition(std::vector<int>&, const int&, const int&);
-std::vector<int> ReadNumbers(const std::string&);
-void WriteAnswer(const std::vector<int>&, const std::string&);
 
 int main() {
     auto a = ReadNumbers("numbers.txt");
     quicksort(a, 0, a.size() - 1);
     WriteAnswer(a, "answer.txt");
-}
-
-std::vector<int> ReadNumbers(const std::string& file_name){
-    std::ifstream is(file_name);
-    std::vector<int> arr;
-    for (int n = 0; is >> n; ) {
-        arr.push_back(n);
-    }
-    return arr;
-}
-
-void WriteAnswer(const std::vector<int>& arr, const std::string& file_name){
-    std::ofstream os(file_name);
-    for (const auto& n: arr){
-        os << n << std::endl;
-    }
 }
 
 void quicksort(std::vector<int>& arr, const int& lo, const int& hi) {
@@ -49,10 +29,4 @@ int partition(std::vector<int>& arr, const int& lo, const int& hi) {
     }
     swap(arr, ++i, hi);
     return i;
-}
-
-void swap(std::vector<int>& arr, const int& i, const int& j) {
-    int tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
 }
---
+++ utils.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include <string>
+
+std::vector<int> ReadNumbers(const std::string&);
+void WriteAnswer(const std::vector<int>&, const std::string&);
+void swap(std::vector<int>&, const int&, const int&);
